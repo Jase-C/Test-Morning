3bfdf77a795bb72ed0120fe66a9ba7e183c70566
Brentyn Morgan <35702052+20Dcpenland@users.noreply.github.com>
2020-08-28 09:53:24 -0500

    First commit

---
+++ JCorwin Test Morning/JCorwin Test Morning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JCorwin_Test_Morning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
